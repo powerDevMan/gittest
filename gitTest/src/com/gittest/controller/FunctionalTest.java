f22f66c4447e7b3b346bbc6fc76768a1e18aaa47
GDJ47 <GDJ47@192.168.0.174>
2022-06-09 16:16:08 +0900

    goodee_04 getGoodee메소드 추가

--- gitTest/src/com/gittest/controller/FunctionalTest.java
+++ gitTest/src/com/gittest/controller/FunctionalTest.java
@@ -3,9 +3,14 @@ package com.gittest.controller;
 public class FunctionalTest {
 
 	public void printTest() {
-				String name="test";
+		String name="test";
+			
+				
 	}
-	
+	public String getGoodee() {
+		return "goodee";
+		
+	}
 	
 	
 	
